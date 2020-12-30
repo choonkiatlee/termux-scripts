1ac0736b7b0c497e4ff35b12597681c19c6dd6b5
CK <choonkiat.lee@gmail.com>
2020-12-30 22:34:59 +0800

    add dir command

--- display_ip_addr_notif.sh
+++ display_ip_addr_notif.sh
@@ -2,15 +2,15 @@
 
 # Print only our current ip address
 IP_ADDR=`ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'`
-
+dir="$(dirname "$(readlink -f "$0")")"
 termux-notification --id "termux_ip_addr_notif" \
                     --title "SSH Server Running. Current IP Address: " \
                     --content $IP_ADDR \
                     --priority high \
                     --sound \
                     --vibrate 500,1000 \
-                    --on-delete "bash ~/kill_ssh_process.sh" \
+                    --on-delete "bash $dir/kill_ssh_process.sh" \
                     --button1 "Kill SSH Connection" \
-                    --button1-action "bash ~/kill_ssh_process.sh; termux-notification-remove termux_ip_addr_notif"
+                    --button1-action "bash $dir/kill_ssh_process.sh; termux-notification-remove termux_ip_addr_notif"
 
                     # --action "termux-toast $IP_ADDR" 
